c56094448a3c2fde549edc0e5ab7991c0b260181
susmita-karkii <85597124+susmita-karkii@users.noreply.github.com>
2023-06-27 21:56:13 +0545

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -84,9 +84,16 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': os.environ.get('rds_endpoint'),
+        'NAME': 'mydb',
+        'USER': os.environ.get('user'),
+        'PASSWORD': os.environ.get('password'),
+        'PORT': 3306,
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+}
 }
 
 
